c493e380436242507c975ebcd6dd9a7e1a0795f1
donga <dongan0618@naver.com>
2024-05-24 23:19:38 +0900

    app update

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,15 @@
 const express=require('express');
 const app=express();
-app.listen(3000,()=>{
-    console.log("3000번에서 현재 대기중입니다.")
+const dotenv=require("dotenv");
+const indexRouter=require('./routes/index');
+const {urlencoded} = require("express");
+dotenv.config()
+
+app.use(express.json())
+app.use(urlencoded({extended:true}))
+
+app.use('/',indexRouter);
+
+app.listen(process.env.port,()=>{
+    console.log(process.env.port+"번에서 현재 대기중입니다.")
 })
