e73396f1dbef0242c6e3449950a2409f0b58ecf2
donga <dongan0618@naver.com>
2024-05-28 21:38:07 +0900

    front-end up

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h5>블로그임</h5>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
